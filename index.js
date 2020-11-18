f902c3fc180af26623045bec9510ce36180b081a
Aquiles Mariani <aquiles.mariani@radiumrocket.com>
2020-11-18 01:54:06 -0300

    express with static, dynamic and rest API routes

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-var uniq = require('lodash.uniq');
+const express = require('express');
+const app = express();
+const port = 3000;
+const getUniqueNumbers = require('./api/index');
 
-const randomNumbers = [0, 2, 5, 8, 98, 8, 55, 1, 2];
 
-const uniqueNumbers = uniq(randomNumbers);
-console.log(uniqueNumbers);
+// Static Files
+app.use(express.static('public'));
+
+// Dynamic render
+app.get('/dynamic', function (req, res) {
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>${new Date()}</h1>
+  </body>
+  </html>`);
+});
+
+// REST API
+app.get('/unique', (req, res) => {
+  res.send(getUniqueNumbers());
+});
+
+
+app.listen(port, () => {
+  console.log(`Radium app listening at http://localhost:${port}`);
+});
